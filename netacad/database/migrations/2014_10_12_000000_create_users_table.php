785281d46f1cd050ef40c3002512c9f2e51ac6ad
Teddy <tkoeniadicommunic8agency@gmail.com>
2019-05-08 09:50:33 +0700

    remove email field on user table

--- netacad/database/migrations/2014_10_12_000000_create_users_table.php
+++ netacad/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('nim', 7);
-            $table->string('email')->unique();
+            // $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('level')->nullable();
--- netacad/database/seeds/UserTableSeeder.php
+++ netacad/database/seeds/UserTableSeeder.php
@@ -14,21 +14,21 @@ class UserTableSeeder extends Seeder
         DB::table('users')->insert([
             'nim' => '1231231',
             'name' => 'admin',
-            'email' => 'admin@admin.com',
+            // 'email' => 'admin@admin.com',
             'password' => bcrypt('adminadmin'),
             'level' => 0,
         ]);
         DB::table('users')->insert([
             'nim' => '1501702',
             'name' => 'Ziady Mubaraq',
-            'email' => 'ziady@gmail.com',
+            // 'email' => 'ziady@gmail.com',
             'password' => bcrypt('adminadmin'),
             'level' => 1,
         ]);
         DB::table('users')->insert([
             'nim' => '1501702',
             'name' => 'Desphi Satria',
-            'email' => 'kuya@gmail.com',
+            // 'email' => 'kuya@gmail.com',
             'password' => bcrypt('adminadmin'),
             'level' => 1,
         ]);
