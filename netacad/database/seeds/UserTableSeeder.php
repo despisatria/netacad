04496bc2a8aaf182d7214006a9aaea7f71f34d40
despi_satria <despi.satria@gmail.com>
2019-05-08 19:41:46 +0700

    tambah user

--- netacad/database/seeds/UserTableSeeder.php
+++ netacad/database/seeds/UserTableSeeder.php
@@ -18,13 +18,7 @@ class UserTableSeeder extends Seeder
             'password' => bcrypt('adminadmin'),
             'level' => 0,
         ]);
-        DB::table('users')->insert([
-            'nim' => '1501702',
-            'name' => 'Ziady Mubaraq',
-            // 'email' => 'ziady@gmail.com',
-            'password' => bcrypt('adminadmin'),
-            'level' => 1,
-        ]);
+
         DB::table('users')->insert([
             'nim' => '1501702',
             'name' => 'Desphi Satria',
@@ -32,5 +26,85 @@ class UserTableSeeder extends Seeder
             'password' => bcrypt('adminadmin'),
             'level' => 1,
         ]);
+
+        DB::table('users')->insert([
+            'nim' => '1700916',
+            'name' => 'DesphiSatria',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('adminadmin'),
+            'level' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'nim' => '1705008',
+            'name' => 'Althea Rizqi A',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1705008'),
+            'level' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'nim' => '1703861',
+            'name' => 'Andita Khairunnisa',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1703861'),
+            'level' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'nim' => '1701499',
+            'name' => 'Ani Mulyani',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1701499'),
+            'level' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'nim' => '1700701',
+            'name' => 'Annisa F',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1700701'),
+            'level' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'nim' => '1705408',
+            'name' => 'Bisma Pandara S',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1705408'),
+            'level' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'nim' => '1703734',
+            'name' => 'Bobby M Ilham',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1703734'),
+            'level' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'nim' => '1704734',
+            'name' => 'Dela Adelia',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1704734'),
+            'level' => 1,
+        ]); 
+
+        DB::table('users')->insert([
+            'nim' => '1703571',
+            'name' => 'Dina Dwi H ',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1703571'),
+            'level' => 1,
+        ]); 
+
+        DB::table('users')->insert([
+            'nim' => '1702329',
+            'name' => 'Fachrul Rusmayadi ',
+            // 'email' => 'kuya@gmail.com',
+            'password' => bcrypt('1702329'),
+            'level' => 1,
+        ]); 
     }
 }
